c996b0a767de039db0f2011891110f60bf8a8bd1
Libor Jonat <libor.jonat@quantlane.com>
2024-05-09 22:04:24 +0200

    WIP added admins

--- src/money_project/account/admin.py
+++ src/money_project/account/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
+from models import (
+    BaseExpenseModel,
+    CategoryModel,
+    CurrencyModel,
+    ExtraExpenseModel,
+    RegularExpenseModel,
+    TagModel,
+    ManualAccountStateModel,
+    MoneyAccountModel,
+)
+
+
 # Register your models here.
+
+
+@admin.register(BaseExpenseModel)
+class BaseExpenseModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CurrencyModel)
+class CurrencyModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExtraExpenseModel)
+class ExtraExpenseModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RegularExpenseModel)
+class RegularExpenseModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TagModel)
+class TagModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ManualAccountStateModel)
+class ManualAccountStateModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MoneyAccountModel)
+class MoneyAccountModelAdmin(admin.ModelAdmin):
+    pass
--- src/money_project/account/models/__init__.py
+++ src/money_project/account/models/__init__.py
@@ -1,1 +1,4 @@
-from account import MoneyAccountModel
+from account import MoneyAccountModel, ManualAccountStateModel
+from base import BaseExpenseModel, TagModel, CurrencyModel, CategoryModel
+from extra import ExtraExpenseModel
+from regular import RegularExpenseModel
